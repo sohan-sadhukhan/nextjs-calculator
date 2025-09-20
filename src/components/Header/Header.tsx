a0e81cdbe5915986714fd273df03580207679c0d
New-Sohan <mrsohansadhukhan@gmail.com>
2025-09-20 20:45:41 +0530

    project setup completed

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import ThemeToggleButton from "../customui/ThemeToggleButton";
+
+const Header = () => {
+	return (
+		<header
+			className="border-b shadow"
+			aria-label="app-header">
+			<div className="container mx-auto flex items-center justify-between px-6 py-3">
+				<Link href={"/"}>
+					<h1
+						className="text-2xl font-semibold"
+						aria-label="App Name">
+						<span className="font-semibold text-white/90">
+							Nextjs
+						</span>
+						Calculator
+					</h1>
+				</Link>
+
+				<nav className="flex items-center gap-4">
+					<Link href={"/"}>Home</Link>
+
+					<ThemeToggleButton />
+				</nav>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
