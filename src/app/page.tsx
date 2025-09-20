36753ba72e8d8ebfdfcd739c3d9c2dfc878c278b
New-Sohan <mrsohansadhukhan@gmail.com>
2025-09-21 01:03:30 +0530

    basic calculator created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
+import BasicCalculator from "@/components/BasicCalculator";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@/components/shadcnui/card";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Home | Nextjs Starter Template",
-	description: "Home page of Nextjs Starter Template",
+	title: "Basic | Nextjs Calculator",
+	description: "Basic page of Nextjs Calculator",
 };
 
 const page = () => {
 	return (
 		<>
-			<section className="grid h-[80dvh] place-items-center"></section>
+			<section className="grid h-[80dvh] place-items-center">
+				<Card>
+					<CardHeader>
+						<CardTitle className="grid place-items-center text-2xl">
+							Basic Calculator
+						</CardTitle>
+					</CardHeader>
+
+					<CardContent>
+						<BasicCalculator />
+					</CardContent>
+				</Card>
+			</section>
 		</>
 	);
 };
---
+++ src/components/BasicCalculator.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./shadcnui/button";
+import { Input } from "./shadcnui/input";
+
+const BasicCalculator = () => {
+	const [Number1, setNumber1] = useState("");
+	const [Number2, setNumber2] = useState("");
+	const [total, setTotal] = useState("");
+
+	const handleAdd = () => {
+		if (Number1 !== "" && Number2 !== "") {
+			const result = parseInt(Number1) + parseInt(Number2);
+			setTotal(result.toString());
+		}
+
+		setNumber1("");
+		setNumber2("");
+	};
+
+	const handleSubtract = () => {
+		if (Number1 !== "" && Number2 !== "") {
+			const result = parseInt(Number1) - parseInt(Number2);
+			setTotal(result.toString());
+		}
+
+		setNumber1("");
+		setNumber2("");
+	};
+
+	const handleMultiply = () => {
+		if (Number1 !== "" && Number2 !== "") {
+			const result = parseInt(Number1) * parseInt(Number2);
+			setTotal(result.toString());
+		}
+
+		setNumber1("");
+		setNumber2("");
+	};
+
+	const handleDevide = () => {
+		if (Number1 !== "" && Number2 !== "") {
+			const result = parseInt(Number1) / parseInt(Number2);
+			setTotal(result.toString());
+		}
+
+		setNumber1("");
+		setNumber2("");
+	};
+
+	const handleReset = () => {
+		setNumber1("");
+		setNumber2("");
+		setTotal("");
+	};
+	return (
+		<div className="grid grid-cols-4 gap-4">
+			<Input
+				type="number"
+				className="col-span-2"
+				placeholder="Number 1"
+				onChange={(e) => setNumber1(e.target.value)}
+				value={Number1}
+			/>
+			<Input
+				type="number"
+				className="col-span-2"
+				placeholder="Number 2"
+				onChange={(e) => setNumber2(e.target.value)}
+				value={Number2}
+			/>
+			<Input
+				type="number"
+				className="col-span-4"
+				placeholder="Total"
+				disabled
+				value={total}
+			/>
+
+			<Button
+				type="button"
+				onClick={handleAdd}
+				disabled={!Number1 || !Number2}
+				className="cursor-pointer">
+				Add
+			</Button>
+			<Button
+				type="button"
+				onClick={handleSubtract}
+				disabled={!Number1 || !Number2}
+				className="cursor-pointer">
+				Subtract
+			</Button>
+			<Button
+				type="button"
+				onClick={handleMultiply}
+				disabled={!Number1 || !Number2}
+				className="cursor-pointer">
+				Multiply
+			</Button>
+			<Button
+				type="button"
+				onClick={handleDevide}
+				disabled={!Number1 || !Number2}
+				className="cursor-pointer">
+				Devide
+			</Button>
+
+			<Button
+				type="button"
+				onClick={handleReset}
+				className="col-span-4 cursor-pointer">
+				Reset
+			</Button>
+		</div>
+	);
+};
+
+export default BasicCalculator;
